814b83feead8baaa330555939f37d2291edc1f87
tim <tim@ibears.io>
2024-05-03 17:37:48 +0800

    add runnable sequence

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { program } = require('commander');
 const ExcelJS = require('exceljs');
 const pLimit = require('p-limit');
-
 const langChain = require('./lib/langChain');
 
 program
@@ -19,14 +18,31 @@ program
 program.parse(process.argv);
 
 const prompt = '你是一位客服，需要回覆使用者提出的問題。';
-const input = '忘記密碼';
+const inputs = ['我忘記密碼了','是登錄密碼'];
+const res = []
 
 async function testRGA() {
 	const model = langChain.createLLM();
 	const file = await langChain.loadJsonFile("file/20240422-data-text-clean.json");
-	const vectorStore = await langChain.createVectorStore(file);
-	const retrievalChain = await langChain.createChain(model, vectorStore, prompt);
-	const res = await retrievalChain.invoke({ input });
+	const retriever = await langChain.createRetriever(file);
+	// const retrievalChain = await langChain.createChain(model, retriever, prompt);
+	// const res = await retrievalChain.invoke({ input });
+	const conversationChain = await langChain.createConversationRetrievalQA(model, retriever, prompt);
+
+	for (let i=0; i < inputs.length; ++i){
+		let response;
+		if(i===0){
+			response = await conversationChain.invoke({ 
+				question: inputs[i],
+			});
+		}else{
+			response = await conversationChain.invoke({ 
+				question: inputs[i],
+				chatHistory: langChain.formatChatHistory(inputs[i-1], res[i-1]),
+			});
+		}
+		res.push(response)
+	}
 
 	console.log(res)
 }
